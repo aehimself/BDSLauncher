b29bc0247d065d84f7c9642abd99b61046bf26f6
AEHimself <57598335+aehimself@users.noreply.github.com>
2022-12-31 20:50:12 +0100

    Taking over file associations should keep the original description and icon if possible

--- uFileAssociations.pas
+++ uFileAssociations.pas
@@ -21,18 +21,30 @@ Const
   CLASSESROOT = '\SOFTWARE\Classes\';
   AEBDSLAUNCHERROOT = CLASSESROOT + 'AEBDSLauncher';
   BDSLAUNCHERBACKUP = 'AEBDSLauncherBackup';
-  CHANGEEXTENSIONS : Array[0..4] Of String = ('.pas', '.dpr', '.dproj', '.dfm', '.groupproj');
+  CHANGEEXTENSIONS : Array[0..5] Of String = ('.pas', '.dpr', '.dproj', '.dfm', '.groupproj', '.dpk');
 
 Procedure NotifyAssociationsChange;
 Begin
   SHChangeNotify(SHCNE_ASSOCCHANGED, SHCNF_IDLIST, nil, nil);
 End;
 
-Procedure ResetFileAssociation(Const inRegistry: TRegistry; Const inPath: String);
+//
+// Giving back file associations
+//
+
+Procedure InternalGiveBackFileAssociations(Const inRegistry: TRegistry; Const inPath: String);
 Var
-  delkey: Boolean;
+  rootkeyfound, delkey: Boolean;
 Begin
+  // Revert taken over associations back to their previous ones (if there were any). If there was no association before taking over
+  // we have to delete the keys, but reverting twice in a row should not do anything!
+
   delkey := False;
+  rootkeyfound := inRegistry.KeyExists(AEBDSLAUNCHERROOT + inPath);
+
+  // If our key exists, delete it
+  If rootkeyfound Then
+    inRegistry.DeleteKey(AEBDSLAUNCHERROOT + inPath);
 
   If inRegistry.OpenKey(CLASSESROOT + inPath, False) Then
   Try
@@ -41,9 +53,14 @@ Begin
       inRegistry.WriteString('', inRegistry.ReadString(BDSLAUNCHERBACKUP));
 
       inRegistry.DeleteValue(BDSLAUNCHERBACKUP);
+
+      // If backup value exists there was an association for sure, therefore the association must not be deleted, no matter what
+      delkey := False;
     End
     Else
-      delkey := True;
+      // No backup key was found. This can mean there was no previous association OR we are reverting from a non-taken-over
+      // state. Therefore, association only has to be deleted if our key was found!
+      delkey := rootkeyfound;
   Finally
     inRegistry.CloseKey;
   End;
@@ -61,17 +78,8 @@ Begin
   Try
     reg.RootKey := HKEY_CURRENT_USER;
 
-    // If AEBDSLauncher does not exist, we did not take file associations over yet. In this case, do nothing!
-    If Not reg.KeyExists(AEBDSLAUNCHERROOT) Then
-      Exit;
-
-    If Not reg.DeleteKey(AEBDSLAUNCHERROOT) Then
-    Begin
-      Exit;
-    End;
-
     For extension In CHANGEEXTENSIONS Do
-      ResetFileAssociation(reg, extension);
+      InternalGiveBackFileAssociations(reg, extension);
 
     NotifyAssociationsChange;
   Finally
@@ -79,13 +87,80 @@ Begin
   End;
 End;
 
-Procedure ChangeFileAssociation(Const inRegistry: TRegistry; Const inPath: String);
+//
+// Taking over file associations
+//
+
+Procedure InternalTakeOverFileAssociations(Const inRegistry: TRegistry; Const inPath: String);
+Var
+  oldtarget, description, friendlytname, icon: String;
 Begin
+  // There are only two things we should consider here: taking over the second time should have zero effect whatsoever
+  // and maybe there are no associations for this file type yet.
+
+  // Open the current file extension assignment, back up the old association and change it to the new one
   If inRegistry.OpenKey(CLASSESROOT + inPath, True) Then
   Try
+    oldtarget := '';
+
+    If Not inRegistry.ValueExists(BDSLAUNCHERBACKUP) Then
+    Begin
+      If inRegistry.ValueExists('') Then
+      Begin
+        oldtarget := inRegistry.ReadString('');
+        inRegistry.WriteString(BDSLAUNCHERBACKUP, oldtarget);
+      End;
+
+      inRegistry.WriteString('', 'AEBDSLauncher' + inPath);
+    End;
+  Finally
+    inRegistry.CloseKey;
+  End;
+
+  If inRegistry.KeyExists(AEBDSLAUNCHERROOT + inPath) Then
+    Exit;
+
+  // Look for the old association and extract description, friendly type name and icon to use
+  description := '';
+  friendlytname := '';
+  If inRegistry.OpenKey(CLASSESROOT + oldtarget, False) Then
+  Try
     If inRegistry.ValueExists('') Then
-      inRegistry.WriteString(BDSLAUNCHERBACKUP, inRegistry.ReadString(''));
-    inRegistry.WriteString('', 'AEBDSLauncher');
+      description := inRegistry.ReadString('');
+
+    If inRegistry.ValueExists('FriendlyTypeName') Then
+      friendlytname := inRegistry.ReadString('FriendlyTypeName');
+  Finally
+    inRegistry.CloseKey;
+  End;
+
+  icon := '';
+  If inRegistry.OpenKey(CLASSESROOT + oldtarget + '\DefaultIcon', False) Then
+  Try
+    If inRegistry.ValueExists('') Then
+      icon := inRegistry.ReadString('');
+  Finally
+    inRegistry.CloseKey;
+  End;
+
+  // Now, create our own association with the previously collected information
+  If description.IsEmpty Then
+    description := 'AE BDSLauncher file';
+  If inRegistry.OpenKey(AEBDSLAUNCHERROOT + inPath, True) Then
+  Try
+    inRegistry.WriteString('', description);
+
+    If Not friendlytname.IsEmpty Then
+      inRegistry.WriteString('FriendlyTypeName', friendlytname);
+  Finally
+    inRegistry.CloseKey;
+  End;
+
+  If icon.IsEmpty Then
+    icon := ParamStr(0);
+  If inRegistry.OpenKey(AEBDSLAUNCHERROOT + inPath+ '\DefaultIcon', True) Then
+  Try
+    inRegistry.WriteString('', icon);
   Finally
     inRegistry.CloseKey;
   End;
@@ -100,29 +175,8 @@ Begin
   Try
     reg.RootKey := HKEY_CURRENT_USER;
 
-    If reg.OpenKey(AEBDSLAUNCHERROOT, True) Then
-    Try
-      reg.WriteString('', 'AE BDSLauncher file');
-    Finally
-      reg.CloseKey;
-    End;
-
-    If reg.OpenKey(AEBDSLAUNCHERROOT + '\DefaultIcon', True) Then
-    Try
-      reg.WriteString('', ParamStr(0));
-    Finally
-      reg.CloseKey;
-    End;
-
-    If reg.OpenKey(AEBDSLAUNCHERROOT + '\shell\open\command', True) Then
-    Try
-      reg.WriteString('', ParamStr(0) + ' "%1"');
-    Finally
-      reg.CloseKey;
-    End;
-
     For extension In CHANGEEXTENSIONS Do
-      ChangeFileAssociation(reg, extension);
+      InternalTakeOverFileAssociations(reg, extension);
 
     NotifyAssociationsChange;
   Finally
